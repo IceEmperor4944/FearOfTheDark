d0d7a77d35b22504bfa782f4eb71f9ba602ee3c5
IceEmperor4944 <macaroni49344@gmail.com>
2026-02-17 09:38:44 -0700

    Squashed commit of the following:
    commit 151d76f3b00857fadd1d46338d6faeb18e936933
    Author: IceEmperor4944 <macaroni49344@gmail.com>
    Date:   Mon Feb 16 18:07:28 2026 -0700
    
        Finalized Door System

--- Assets/Nates_Junk/Scripts/Interactible.cs
+++ Assets/Nates_Junk/Scripts/Interactible.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Interactible : MonoBehaviour
 {
-    public void Interact(GameObject other)
+    public virtual void Interact(GameObject other)
     {
         print("interacted");
     }
