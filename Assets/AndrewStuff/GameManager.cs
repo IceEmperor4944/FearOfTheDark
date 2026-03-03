907b82300d6ec3b19f7945b0d9fcb31acab46400
IceEmperor4944 <macaroni49344@gmail.com>
2026-03-03 14:51:41 -0700

    Please be the last change

--- Assets/AndrewStuff/GameManager.cs
+++ Assets/AndrewStuff/GameManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,38 +8,35 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject gameUI;
     [SerializeField] GameObject winUI;
     [SerializeField] TMP_Text notesObject;
-
     [HideInInspector] public int notesCollected = 0;
 
-    static GameManager instance;
-    public static GameManager Instacnce { get { return instance; } }
-
-    enum GameState
-    {
-        MainMenu,
-        Playing,
-        NoteOpened,
-        Complete
-    }
-
-    GameState currentState = GameState.MainMenu;
+    [HideInInspector] public enum GameState { MainMenu, Playing, NoteOpen, Complete }
+    [HideInInspector] public GameState currentState = GameState.MainMenu;
 
     void Update()
     {
-       
         notesObject.text = "Notes Collected: " + notesCollected + "/6";
 
         switch (currentState)
         {
             case GameState.MainMenu:
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
                 titleUI.SetActive(true);
                 gameUI.SetActive(false);
                 winUI.SetActive(false);
                 break;
             case GameState.Playing:
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
                 titleUI.SetActive(false);
                 gameUI.SetActive(true);
                 winUI.SetActive(false);
+                break;
+            case GameState.NoteOpen:
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                gameUI.SetActive(false);
                 break;
             case GameState.Complete:
                 titleUI.SetActive(false);
--- Assets/Nates_Junk/Scripts/UserInterface.cs
+++ Assets/Nates_Junk/Scripts/UserInterface.cs
@@ -4,28 +4,22 @@ using TMPro;
 public class UserInterface : MonoBehaviour
 {
     [SerializeField] GameObject noteFrame;
-
-    [SerializeField] GameObject gameManager;
+    [SerializeField] TMP_Text noteText;
+    [SerializeField] GameManager gameManager;
 
     public void DisplayNote(string text)
     {
-        var noteText = noteFrame.GetComponentInChildren<TextMeshPro>();
-        if (noteText != null)
-        {
-            noteFrame.SetActive(true);
+        noteFrame.SetActive(true);
 
-            if (noteText.TryGetComponent(out TextMeshPro textMesh))
-            {
-                textMesh.text = text;
-                gameManager.TryGetComponent(out GameManager manager);
-                manager.notesCollected++;
-            }
-        }
+        noteText.text = text;
+        gameManager.notesCollected++;
+        gameManager.currentState = GameManager.GameState.NoteOpen;
     }
 
     public void CloseNote()
     {
         noteFrame.SetActive(false);
+        gameManager.currentState = GameManager.GameState.Playing;
     }
 
     public void EndGame()
