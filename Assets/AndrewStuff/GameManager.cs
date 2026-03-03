31d813954bcc57fe32940bc35a819826093c4c3f
IceEmperor4944 <macaroni49344@gmail.com>
2026-03-03 14:02:02 -0700

    Squashed commit of the following:
    commit d19ec7c8ce9181a6f050eb8e6b80d3310fef4bf2
    Author: AJGamess <ajacoboski@student.neumont.edu>
    Date:   Tue Mar 3 13:58:06 2026 -0700
    
        Updated UI
    
    commit e5ffea07ad3afe24d2c052fcb27ced98a9c3b651
    Author: AJGamess <ajacoboski@student.neumont.edu>
    Date:   Tue Mar 3 09:54:24 2026 -0700
    
        Gitignore stuff
    
    commit d3c1a308a6d9f597751b8b7068edca382160c2b8
    Merge: 7f63b12 6505a8b
    Author: AJGamess <158221005+AJGamess@users.noreply.github.com>
    Date:   Tue Mar 3 09:52:31 2026 -0700
    
        Merge pull request #26 from IceEmperor4944/master
    
        Pulling From Master
    
    commit 7f63b12958a5d766aa51c2aea087f75ca9ad9f5a
    Merge: c16bf1b c9319e0
    Author: AJGamess <158221005+AJGamess@users.noreply.github.com>
    Date:   Tue Mar 3 09:16:10 2026 -0700
    
        Merge pull request #24 from IceEmperor4944/master
    
        Pulling from Master

--- Assets/AndrewStuff/GameManager.cs
+++ Assets/AndrewStuff/GameManager.cs
@@ -1,12 +1,17 @@
+using System.Collections;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject titleUI;
     [SerializeField] GameObject gameUI;
     [SerializeField] GameObject winUI;
-    [SerializeField] GameObject settingsUI;
+    [SerializeField] TMP_Text notesObject;
+
+    [HideInInspector] public int notesCollected = 0;
 
     static GameManager instance;
     public static GameManager Instacnce { get { return instance; } }
@@ -15,7 +20,7 @@ public class GameManager : MonoBehaviour
     {
         MainMenu,
         Playing,
-        Settings,
+        NoteOpened,
         Complete
     }
 
@@ -23,42 +28,42 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+       
+        notesObject.text = "Notes Collected: " + notesCollected + "/6";
+
         switch (currentState)
         {
             case GameState.MainMenu:
                 titleUI.SetActive(true);
                 gameUI.SetActive(false);
                 winUI.SetActive(false);
-                settingsUI.SetActive(false);
                 break;
             case GameState.Playing:
                 titleUI.SetActive(false);
                 gameUI.SetActive(true);
                 winUI.SetActive(false);
-                settingsUI.SetActive(false);
-                break;
-            case GameState.Settings:
-                titleUI.SetActive(false);
-                gameUI.SetActive(false);
-                winUI.SetActive(false);
-                settingsUI.SetActive(true);
                 break;
             case GameState.Complete:
                 titleUI.SetActive(false);
                 gameUI.SetActive(false);
                 winUI.SetActive(true);
-                settingsUI.SetActive(false);
                 break;
         }
     }
+    IEnumerator ReturnToMainMenu()
+    {
+        yield return new WaitForSeconds(5.0f);
 
+        currentState = GameState.MainMenu;
+    }
+    public void EndGame()
+    {
+        currentState = GameState.Complete;
+        StartCoroutine(ReturnToMainMenu());
+    }
     public void OnStartGame()
     {
         currentState = GameState.Playing;
-    }
-    public void OnSettings()
-    {
-        currentState = GameState.Settings;
     }
     public void OnMainMenu()
     {
